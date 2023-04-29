485f5b16cd5ba90e1723ec16ac7628f2d6105c13
André Nascimento <andre4.20nascimento@outlook.com>
2023-04-29 20:18:50 -0300

    avaliação de Diabetes em grávidas

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿
+Console.Clear();
+
+int mg;
+
+Console.WriteLine("---DiabetesGestacional---\n");
+
+Console.Write("Digite o valor em glicerina: ");
+mg = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+
+if (mg < 92)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"{mg} mg/dL = reavaliar entre 24 e 28 semanas com TTOG 75g de glicose");
+    Console.ResetColor();
+    Environment.Exit(1);
+}
+if (mg >= 126)
+{
+   Console.ForegroundColor = ConsoleColor.Red;
+   Console.WriteLine($"{mg} mg/dL = Diabetes Mellitus na Gravidez");
+   Console.ResetColor();
+   Environment.Exit(1);
+}
+
+ Console.ForegroundColor = ConsoleColor.Yellow;
+ Console.WriteLine($"{mg} mg/dL = Compatível com Diabetes Gestacional");
+ Console.ResetColor();
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # DiabetesGestacional
 programa que receba o valor de glicemia (em mg/dL) e exiba o diagnóstico adequado.
+
+## Exercício `DiabetesGestacional`
+
+Segundo a Sociedade Brasileira de Diabetes¹, a avaliação de Diabetes em grávidas é realizada através de um exame de glicemia comparado com a tabela abaixo:
+
+```
+< 92 mg/dL (reavaliar entre 24 e 28 semanas com TTOG 75g de glicose)
+= ou > 92 mg/dL e < 126 mg/dL = Compatível com Diabetes Gestacional
+= ou > 126 mg/dL = Diabetes Mellitus na Gravidez
+```
+
+Faça um programa que receba o valor de glicemia (em mg/dL) e exiba o diagnóstico adequado.
+
+¹ _International Association of the Diabetes and Pregnancy Study Groups_ (IADPSG), OMS Diretrizes da Sociedade Brasileira de Diabetes 2017-2018.
